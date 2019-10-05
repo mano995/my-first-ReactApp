d145e17852a62672493a476acb8654f75962b4db
m.aminamer <amin.amer@stud.uni-due.de>
2019-10-05 13:14:34 +0200

    firstCommit

---
+++ src/build/precache-manifest.b4e45cc6ccc77c6f323b8354a0bd547d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab675dd05fd6f938e7c5396fa7a4da7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cff9d601f7636213fe6",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "b91520e225956d6aa0a0",
+    "url": "/static/js/2.46641d1e.chunk.js"
+  },
+  {
+    "revision": "7cff9d601f7636213fe6",
+    "url": "/static/js/main.e88bfb82.chunk.js"
+  },
+  {
+    "revision": "caf565ff8d5b396d01d6",
+    "url": "/static/js/runtime-main.e82eb31e.js"
+  }
+]);
